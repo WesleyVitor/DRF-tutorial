06aa8b52314d4129d515b94c9a8b1f653cb098b7
WesleyVitor <wesleydemorais@outlook.com.br>
2022-05-30 21:04:31 -0300

    feat: Finished crud basic

--- courses/urls.py
+++ courses/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from .views import CourseApiView, AvaliationApiView
+from .views import CoursesApiView, AvaliationsApiView, CourseApiView, AvaliationApiView
 
 urlpatterns = [
-    path("", CourseApiView.as_view(), name="courses"),
-    path("avaliations/", AvaliationApiView.as_view(), name="avaliations"),
+    path("", CoursesApiView.as_view(), name="courses"),
+    path("avaliations/", AvaliationsApiView.as_view(), name="avaliations"),
+    path("<int:pk>/", CourseApiView.as_view(), name="course"),
+    path("avaliations/<int:avaliation_pk>/", AvaliationApiView.as_view(), name="avaliation"),
+    path("<int:course_pk>/avaliations/", AvaliationsApiView.as_view(), name="avaliations_by_course"),
+    path("<int:course_pk>/avaliations/<int:avaliation_pk>/", AvaliationApiView.as_view(), name="avaliation_by_course"),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,4 @@
+from rest_framework.generics import  get_object_or_404
 from rest_framework import generics
 
 from .models import Course, Avaliation
@@ -5,13 +6,33 @@ from .models import Course, Avaliation
 from .serializers import AvaliationSerializer, CourseSerializer
 
 
-class CourseApiView(generics.ListCreateAPIView):
+class CoursesApiView(generics.ListCreateAPIView):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
+class CourseApiView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
 
-class AvaliationApiView(generics.ListCreateAPIView):
+class AvaliationsApiView(generics.ListCreateAPIView):
     queryset = Avaliation.objects.all()
     serializer_class = AvaliationSerializer
 
+    def get_queryset(self):
+        if self.kwargs.get("course_pk"): #Verifica se foi passado pela url um valor para course_pk
+            return self.queryset.filter(course_id=self.kwargs.get("course_pk"))
+        return self.queryset.all()
+
+class AvaliationApiView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Avaliation.objects.all()
+    serializer_class = AvaliationSerializer
+
+    def get_object(self):
+        
+        if self.kwargs.get("course_pk") and self.kwargs.get("avaliation_pk"):
+            course_pk = self.kwargs.get("course_pk")
+            avaliation_pk = self.kwargs.get("avaliation_pk")
+            return get_object_or_404(self.queryset, course_id=course_pk,pk=avaliation_pk)
+
+        return get_object_or_404(self.queryset, pk=self.kwargs.get("avaliation_pk"))
 
